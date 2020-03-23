c21751150646977489005fa15329a94e2b5921d4
Max Denushev <mdenushev@ya.ru>
2020-03-23 16:55:45 +0700

    genesis.go multisig disable

--- x/multisig/genesis.go
+++ x/multisig/genesis.go
@@ -3,16 +3,12 @@ package multisig
 import (
 	"bitbucket.org/decimalteam/go-node/x/multisig/internal/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	abci "github.com/tendermint/tendermint/abci/types"
 )
 
 // InitGenesis initialize default parameters
 // and the keeper's address to pubkey map
-func InitGenesis(ctx sdk.Context, k Keeper, /* TODO: Define what keepers the module needs */, data types.GenesisState) {
+func InitGenesis(ctx sdk.Context, k Keeper, data types.GenesisState) {
 
-	// TODO: Define logic for when you would like to initalize a new genesis
-
-	return []abci.ValidatorUpdate{}
 }
 
 // ExportGenesis writes the current store values
