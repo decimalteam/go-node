0025d8b20edc5f85927324a0d098304920ff860e
Max Denushev <mdenushev@ya.ru>
2020-03-24 15:19:18 +0700

    update update.sh script

--- ci-scripts/update.sh
+++ ci-scripts/update.sh
@@ -2,7 +2,9 @@
 DECIMALDIR="$HOME/go/src/bitbucket.org/decimalteam/go-node"
 DECIMALGIT="git@bitbucket.org:decimalteam/go-node.git"
 BRANCH="develop"
+SERVICEPATH="/etc/systemd/system/decd.service"
 printf "Decimal folder: %s\n" "$DECIMALDIR"
+
 if [ ! -d $DECIMALDIR ] ; then
   printf "No Decimal found. Cloning...\n"
   mkdir -p $DECIMALDIR
@@ -17,3 +19,21 @@ else
   printf "Pulled. Building...\n"
   make all
 fi
+
+printf "Restrting service..."
+
+if [ ! -d $SERVICEPATH ] ; then
+  printf "No service file found. Creating."
+  sudo touch $SERVICEPATH
+  echo "[Unit]
+Description=Decimal daemon
+
+[Service]
+User=centos
+Type=simple
+ExecStart=decd start" | sudo tee $SERVICEPATH
+  sudo systemctl start decd
+else
+  sudo systemctl restart decd
+fi
+
