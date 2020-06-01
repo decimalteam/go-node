83c18f974505ab1b5bc7d7587eb451ae8812979a
sirkz <golang.freelance@gmail.com>
2020-06-01 12:59:40 +0700

    Remove sequence event

--- x/coin/handler.go
+++ x/coin/handler.go
@@ -42,7 +42,7 @@ func floatFromInt(amount sdk.Int) float64 {
 // NewHandler creates an sdk.Handler for all the coin type messages
 func NewHandler(k Keeper) sdk.Handler {
 	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
-		//ctx = ctx.WithEventManager(sdk.NewEventManager())
+		ctx = ctx.WithEventManager(sdk.NewEventManager())
 		switch msg := msg.(type) {
 		case types.MsgCreateCoin:
 			return handleMsgCreateCoin(ctx, k, msg)
