8c5d1cbf2944e9c101aad2ca673a8baf25dfc745
Igor Sokolov <wardencliffe.sis@gmail.com>
2020-06-10 17:39:40 +0700

    [reset] making reset after some breaking consensus changes

--- config/config.go
+++ config/config.go
@@ -37,7 +37,7 @@ const (
 	DecimalPrefixConsPub = DecimalMainPrefix + PrefixValidator + PrefixConsensus + PrefixPublic
 
 	// ChainID is the Decimal chain identifier.
-	ChainID = "decimal-testnet-06-08-15-00"
+	ChainID = "decimal-testnet-06-10-15-00"
 
 	TitleTestBaseCoin  = "Test decimal coin"
 	SymbolTestBaseCoin = "tdel"
