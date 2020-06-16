759f92e44a6cd48af7d3adf9fb559adc6cc905cf
Igor Sokolov <wardencliffe.sis@gmail.com>
2020-06-16 21:46:29 +0700

    [reset]

--- ci-scripts/update_configs.sh
+++ ci-scripts/update_configs.sh
@@ -9,7 +9,7 @@ echo "Step 1. Clear current state."
 rm $HOME/.decimal -r
 
 echo "Create basic configs."
-decd init $(hostname) --chain-id=decimal-testnet-06-16-14-00
+decd init $(hostname) --chain-id=decimal-testnet-06-16-21-00
 rm -r $HOME/.decimal/daemon/data
 shopt -s extglob
 cd $HOME/.decimal/daemon/config/
--- config/config.go
+++ config/config.go
@@ -37,7 +37,7 @@ const (
 	DecimalPrefixConsPub = DecimalMainPrefix + PrefixValidator + PrefixConsensus + PrefixPublic
 
 	// ChainID is the Decimal chain identifier.
-	ChainID = "decimal-testnet-06-16-14-00"
+	ChainID = "decimal-testnet-06-16-21-00"
 
 	TitleTestBaseCoin  = "Test decimal coin"
 	SymbolTestBaseCoin = "tdel"
