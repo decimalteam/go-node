42707b99db325dc0f2dcb5224af61a11641f3a6b
Igor Sokolov <wardencliffe.sis@gmail.com>
2020-06-30 15:14:51 +0700

    [reset]

--- config/config.go
+++ config/config.go
@@ -37,7 +37,7 @@ const (
 	DecimalPrefixConsPub = DecimalMainPrefix + PrefixValidator + PrefixConsensus + PrefixPublic
 
 	// ChainID is the Decimal chain identifier.
-	ChainID = "decimal-testnet-06-30-14-30"
+	ChainID = "decimal-testnet-06-30-15-00"
 
 	TitleTestBaseCoin  = "Test decimal coin"
 	SymbolTestBaseCoin = "tdel"
