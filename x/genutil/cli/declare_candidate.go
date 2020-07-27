65e2edbcf6f05d96805307691124877806890690
Igor Sokolov <wardencliffe.sis@gmail.com>
2020-07-27 13:21:57 +0700

    Added `deccli gen-declare-candidate-tx` command useful for generating validators declaration for genesis.json file.

---
+++ x/genutil/cli/declare_candidate.go
@@ -0,0 +1,148 @@
+package cli
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	"github.com/cosmos/cosmos-sdk/client/context"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/codec"
+	kbkeys "github.com/cosmos/cosmos-sdk/crypto/keys"
+	"github.com/cosmos/cosmos-sdk/server"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/cosmos/cosmos-sdk/x/auth"
+	"github.com/cosmos/cosmos-sdk/x/auth/client/utils"
+	"github.com/cosmos/cosmos-sdk/x/genutil/types"
+
+	"bitbucket.org/decimalteam/go-node/x/genutil"
+)
+
+// GenDeclareCandidateTxCmd builds and prints transaction to declare validator candidate.
+// nolint: errcheck
+func GenDeclareCandidateTxCmd(ctx *server.Context, cdc *codec.Codec, mbm module.BasicManager, smbh StakingMsgBuildingHelpers,
+	genAccIterator types.GenesisAccountsIterator, defaultNodeHome, defaultCLIHome string) *cobra.Command {
+
+	ipDefault, _ := server.ExternalIP()
+	fsCreateValidator, flagNodeID, flagPubKey, flagAmount, defaultsDesc := smbh.CreateValidatorMsgHelpers(ipDefault)
+
+	cmd := &cobra.Command{
+		Use:   "gen-declare-candidate-tx",
+		Short: "Generate a genesis declare validator candidate tx carrying a self delegation",
+		Args:  cobra.NoArgs,
+		Long: fmt.Sprintf(`This command is an alias of the 'tx create-validator' command'.
+
+		It creates a genesis transaction to create a validator. 
+		The following default parameters are included: 
+		    %s`, defaultsDesc),
+
+		RunE: func(cmd *cobra.Command, args []string) error {
+			config := ctx.Config
+			config.SetRoot(viper.GetString(flags.FlagHome))
+			nodeID, valPubKey, err := genutil.InitializeNodeValidatorFiles(ctx.Config)
+			if err != nil {
+				return err
+			}
+
+			// Read --nodeID, if empty take it from priv_validator.json
+			if nodeIDString := viper.GetString(flagNodeID); nodeIDString != "" {
+				nodeID = nodeIDString
+			}
+			// Read --pubkey, if empty take it from priv_validator.json
+			if valPubKeyString := viper.GetString(flagPubKey); valPubKeyString != "" {
+				valPubKey, err = sdk.GetPubKeyFromBech32(sdk.Bech32PubKeyTypeConsPub, valPubKeyString)
+				if err != nil {
+					return err
+				}
+			}
+
+			// Read chain ID
+			chainID := viper.GetString(flags.FlagChainID)
+			if len(chainID) == 0 {
+				return fmt.Errorf("chain ID must be specified")
+			}
+
+			_, err = kbkeys.NewKeyring(sdk.KeyringServiceName(), viper.GetString(flags.FlagKeyringBackend), viper.GetString(flagClientHome), cmd.InOrStdin())
+			if err != nil {
+				return err
+			}
+
+			name := viper.GetString(flags.FlagName)
+
+			// Set flags for creating declare validator candidate tx
+			viper.Set(flags.FlagHome, viper.GetString(flagClientHome))
+			smbh.PrepareFlagsForTxCreateValidator(config, nodeID, chainID, valPubKey)
+
+			// Fetch the amount of coins staked
+			amount := viper.GetString(flagAmount)
+			_, err = sdk.ParseCoin(amount)
+			if err != nil {
+				return err
+			}
+
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+			txBldr := auth.NewTxBuilderFromCLI(cliCtx.Input).WithTxEncoder(utils.GetTxEncoder(cdc))
+
+			viper.Set(flags.FlagGenerateOnly, true)
+
+			// create a 'create-validator' message
+			txBldr, msg, err := smbh.BuildCreateValidatorMsg(cliCtx, txBldr)
+			if err != nil {
+				return err
+			}
+
+			info, err := txBldr.Keybase().Get(name)
+			if err != nil {
+				return err
+			}
+
+			if info.GetType() == kbkeys.TypeOffline || info.GetType() == kbkeys.TypeMulti {
+				fmt.Println("Offline key passed in. Use `tx sign` command to sign:")
+				return utils.PrintUnsignedStdTx(txBldr, cliCtx, []sdk.Msg{msg})
+			}
+
+			// write the unsigned transaction to the buffer
+			w := bytes.NewBuffer([]byte{})
+			cliCtx = cliCtx.WithOutput(w)
+
+			if err = utils.PrintUnsignedStdTx(txBldr, cliCtx, []sdk.Msg{msg}); err != nil {
+				return err
+			}
+
+			// read the transaction
+			stdTx, err := readUnsignedGenTxFile(cdc, w)
+			if err != nil {
+				return err
+			}
+
+			// sign the transaction and write it to the output file
+			signedTx, err := utils.SignStdTx(txBldr, cliCtx, name, stdTx, false, true)
+			if err != nil {
+				return err
+			}
+
+			lineBreaker := "================================================================"
+			txJSON, err := cdc.MarshalJSONIndent(signedTx, "", "  ")
+			if err != nil {
+				return err
+			}
+			fmt.Printf(
+				"%s\nPlease, copy following JSON object AS IS to be used in genesis.json file:\n%s\n%s\n%s\n",
+				lineBreaker, lineBreaker, string(txJSON), lineBreaker,
+			)
+			return nil
+
+		},
+	}
+
+	cmd.Flags().String(flags.FlagHome, defaultNodeHome, "node's home directory")
+	cmd.Flags().String(flagClientHome, defaultCLIHome, "client's home directory")
+	cmd.Flags().String(flags.FlagName, "", "name of private key with which to sign the gentx")
+	cmd.Flags().String(flags.FlagChainID, "", "Chain ID for which tx should be signed")
+	cmd.Flags().AddFlagSet(fsCreateValidator)
+
+	return cmd
+}
