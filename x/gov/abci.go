967e7e4a17f356f0e83a40568f62bce067d6b19c
KirillZharkov <sir.k.zharkov@gmail.com>
2020-10-09 13:09:14 +0700

    [update] Fix events

--- x/gov/abci.go
+++ x/gov/abci.go
@@ -12,65 +12,59 @@ func EndBlocker(ctx sdk.Context, keeper Keeper) {
 
 	// delete inactive proposal from store
 	keeper.IterateInactiveProposalsQueue(ctx, uint64(ctx.BlockHeight()), func(proposal Proposal) bool {
-		if proposal.VotingStartBlock == uint64(ctx.BlockHeight()+1) {
-			keeper.RemoveFromInactiveProposalQueue(ctx, proposal.ProposalID, proposal.VotingStartBlock)
-			keeper.InsertActiveProposalQueue(ctx, proposal.ProposalID, proposal.VotingStartBlock)
-			proposal.Status = StatusVotingPeriod
-			keeper.SetProposal(ctx, proposal)
+		keeper.RemoveFromInactiveProposalQueue(ctx, proposal.ProposalID, proposal.VotingStartBlock)
+		keeper.InsertActiveProposalQueue(ctx, proposal.ProposalID, proposal.VotingEndBlock)
+		proposal.Status = StatusVotingPeriod
+		keeper.SetProposal(ctx, proposal)
 
-			ctx.EventManager().EmitEvent(
-				sdk.NewEvent(
-					types.EventTypeInactiveProposal,
-					sdk.NewAttribute(types.AttributeKeyProposalID, fmt.Sprintf("%d", proposal.ProposalID)),
-				),
-			)
-
-			return false
-		}
+		ctx.EventManager().EmitEvent(
+			sdk.NewEvent(
+				types.EventTypeInactiveProposal,
+				sdk.NewAttribute(types.AttributeKeyProposalID, fmt.Sprintf("%d", proposal.ProposalID)),
+			),
+		)
 
 		return false
 	})
 
 	// fetch active proposals whose voting periods have ended (are passed the block time)
 	keeper.IterateActiveProposalsQueue(ctx, uint64(ctx.BlockHeight()), func(proposal Proposal) bool {
-		if proposal.VotingEndBlock == uint64(ctx.BlockHeight()+1) {
-			var tagValue, logMsg string
+		var tagValue, logMsg string
 
-			passes, tallyResults := keeper.Tally(ctx, proposal)
+		passes, tallyResults := keeper.Tally(ctx, proposal)
 
-			if passes {
-				proposal.Status = StatusPassed
-				tagValue = types.AttributeValueProposalPassed
-				logMsg = "passed"
-			} else {
-				proposal.Status = StatusRejected
-				tagValue = types.AttributeValueProposalRejected
-				logMsg = "rejected"
-			}
-
-			proposal.FinalTallyResult = tallyResults
-
-			keeper.SetProposal(ctx, proposal)
-			keeper.RemoveFromActiveProposalQueue(ctx, proposal.ProposalID, proposal.VotingEndBlock)
-
-			logger.Info(
-				fmt.Sprintf(
-					"proposal %d (%s) tallied; result: %s",
-					proposal.ProposalID, proposal.GetTitle(), logMsg,
-				),
-			)
-
-			ctx.EventManager().EmitEvent(
-				sdk.NewEvent(
-					types.EventTypeActiveProposal,
-					sdk.NewAttribute(types.AttributeKeyProposalID, fmt.Sprintf("%d", proposal.ProposalID)),
-					sdk.NewAttribute(types.AttributeKeyProposalResult, tagValue),
-					sdk.NewAttribute(types.AttributeKeyResultVoteYes, tallyResults.Yes.String()),
-					sdk.NewAttribute(types.AttributeKeyResultVoteAbstain, tallyResults.Abstain.String()),
-					sdk.NewAttribute(types.AttributeKeyResultVoteNo, tallyResults.No.String()),
-				),
-			)
+		if passes {
+			proposal.Status = StatusPassed
+			tagValue = types.AttributeValueProposalPassed
+			logMsg = "passed"
+		} else {
+			proposal.Status = StatusRejected
+			tagValue = types.AttributeValueProposalRejected
+			logMsg = "rejected"
 		}
+
+		proposal.FinalTallyResult = tallyResults
+
+		keeper.SetProposal(ctx, proposal)
+		keeper.RemoveFromActiveProposalQueue(ctx, proposal.ProposalID, proposal.VotingEndBlock)
+
+		logger.Info(
+			fmt.Sprintf(
+				"proposal %d (%s) tallied; result: %s",
+				proposal.ProposalID, proposal.GetTitle(), logMsg,
+			),
+		)
+
+		ctx.EventManager().EmitEvent(
+			sdk.NewEvent(
+				types.EventTypeActiveProposal,
+				sdk.NewAttribute(types.AttributeKeyProposalID, fmt.Sprintf("%d", proposal.ProposalID)),
+				sdk.NewAttribute(types.AttributeKeyProposalResult, tagValue),
+				sdk.NewAttribute(types.AttributeKeyResultVoteYes, tallyResults.Yes.String()),
+				sdk.NewAttribute(types.AttributeKeyResultVoteAbstain, tallyResults.Abstain.String()),
+				sdk.NewAttribute(types.AttributeKeyResultVoteNo, tallyResults.No.String()),
+			),
+		)
 		return false
 	})
 }
