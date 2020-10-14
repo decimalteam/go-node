4438d42812924fa58aebdbcf8b4f7e8c3761cbb2
KirillZharkov <sir.k.zharkov@gmail.com>
2020-10-14 12:56:13 +0700

    [update] Increment version

--- config/config.go
+++ config/config.go
@@ -9,7 +9,7 @@ import (
 const (
 
 	// DecimalVersion is integer version of the Decimal app.
-	DecimalVersion = "0.9.13"
+	DecimalVersion = "0.9.14"
 
 	// DecimalMainPrefix is the main prefix for all keys and addresses.
 	DecimalMainPrefix = "dx"
