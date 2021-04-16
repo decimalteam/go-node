99bc5e9a73686005a450b45eff127cf531544a3d
sirkz <golang.freelance@gmail.com>
2021-04-16 13:37:06 +0700

    [reset]

--- config/config.go
+++ config/config.go
@@ -9,7 +9,7 @@ import (
 const (
 
 	// DecimalVersion is integer version of the Decimal app.
-	DecimalVersion = "0.9.18"
+	DecimalVersion = "0.9.19"
 
 	// DecimalMainPrefix is the main prefix for all keys and addresses.
 	DecimalMainPrefix = "dx"
