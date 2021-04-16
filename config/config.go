f9d7c315c531e6a7108f8baae134d7928a378dd6
sirkz <golang.freelance@gmail.com>
2021-04-16 12:36:05 +0700

    [update] NFT fixes

--- config/config.go
+++ config/config.go
@@ -9,7 +9,7 @@ import (
 const (
 
 	// DecimalVersion is integer version of the Decimal app.
-	DecimalVersion = "0.9.17"
+	DecimalVersion = "0.9.18"
 
 	// DecimalMainPrefix is the main prefix for all keys and addresses.
 	DecimalMainPrefix = "dx"
