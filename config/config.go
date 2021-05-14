f3ba6c7eb2587c3aebf5cf25d8de1ee543550ff5
sirkz <golang.freelance@gmail.com>
2021-05-14 16:07:13 +0700

    [update] Fix querier for delegations requests

--- config/config.go
+++ config/config.go
@@ -9,7 +9,7 @@ import (
 const (
 
 	// DecimalVersion is integer version of the Decimal app.
-	DecimalVersion = "1.2.1"
+	DecimalVersion = "1.2.2"
 
 	// DecimalMainPrefix is the main prefix for all keys and addresses.
 	DecimalMainPrefix = "dx"
