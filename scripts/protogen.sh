30a0990392042185c6b0485de27d4091f8d2f07c
igrshalygin <igrshalygin@gmail.com>
2021-05-21 13:55:20 +0700

    removed stale script

--- scripts/protogen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-proto_dirs=$(find ./proto -name '*.proto' -print0 | xargs -0 -n1 dirname | uniq)
-
-for dir in $proto_dirs; do
-  buf protoc $(find "${dir}" -maxdepth 1 -name "*.proto")
-done
