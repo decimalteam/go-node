49b10287840a94d6472304f1840e650773c19881
Михаил В <mvvershinin@gmail.com>
2021-05-25 19:54:24 +0700

    multisign errors

--- x/multisig/internal/types/errors.go
+++ x/multisig/internal/types/errors.go
@@ -1,9 +1,11 @@
 package types
 
 import (
+	"encoding/json"
 	"fmt"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"strconv"
 )
 
 // CodeType defines the local code type.
@@ -27,76 +29,153 @@ const (
 )
 
 func ErrInvalidSender() *sdkerrors.Error {
+	jsonData, _ := json.Marshal(
+		map[string]string{
+			"code":      getCodeString(CodeInvalidSender),
+			"codespace": DefaultCodespace,
+			"desc":      "Invalid sender address: sender address cannot be empty",
+		},
+	)
 	return sdkerrors.New(
 		DefaultCodespace,
 		CodeInvalidSender,
-		"Invalid sender address: sender address cannot be empty",
+		string(jsonData),
 	)
 }
 
-func ErrInvalidOwnerCount(count int, more bool) *sdkerrors.Error {
+func ErrInvalidOwnerCount(more bool) *sdkerrors.Error {
 	var AppendWord string = "need at least"
+	ownerCount := MinOwnerCount
 	if more {
 		AppendWord = "allowed no more"
+		ownerCount = MaxOwnerCount
 	}
+	jsonData, _ := json.Marshal(
+		map[string]string{
+			"code":       getCodeString(CodeInvalidOwnerCount),
+			"codespace":  DefaultCodespace,
+			"desc":       fmt.Sprintf("Invalid owner count: %s %d owners", AppendWord, ownerCount),
+			"AppendWord": AppendWord,
+		},
+	)
 	return sdkerrors.New(
 		DefaultCodespace,
 		CodeInvalidOwnerCount,
-		fmt.Sprintf("Invalid owner count: %s %d owners", AppendWord, MinOwnerCount),
+		string(jsonData),
 	)
 }
 
 func ErrInvalidOwner() *sdkerrors.Error {
+	jsonData, _ := json.Marshal(
+		map[string]string{
+			"code":      getCodeString(CodeInvalidOwner),
+			"codespace": DefaultCodespace,
+			"desc":      "Invalid owner address: owner address cannot be empty",
+		},
+	)
 	return sdkerrors.New(
 		DefaultCodespace,
 		CodeInvalidOwner,
-		"Invalid owner address: owner address cannot be empty",
+		string(jsonData),
 	)
 }
 
 func ErrInvalidWeightCount(LenMsgWeights int, LenMsgOwners int) *sdkerrors.Error {
+	jsonData, _ := json.Marshal(
+		map[string]string{
+			"code":          getCodeString(CodeInvalidWeightCount),
+			"codespace":     DefaultCodespace,
+			"desc":          fmt.Sprintf("Invalid weight count: weight count (%d) is not equal to owner count (%d)", LenMsgWeights, LenMsgOwners),
+			"LenMsgWeights": fmt.Sprintf("%d", LenMsgWeights),
+			"LenMsgOwners":  fmt.Sprintf("%d", LenMsgOwners),
+		},
+	)
 	return sdkerrors.New(
 		DefaultCodespace,
 		CodeInvalidWeightCount,
-		fmt.Sprintf("Invalid weight count: weight count (%d) is not equal to owner count (%d)", LenMsgWeights, LenMsgOwners),
+		string(jsonData),
 	)
 }
 
 func ErrInvalidWeight(weight int, data string) *sdkerrors.Error {
+	jsonData, _ := json.Marshal(
+		map[string]string{
+			"code":      getCodeString(CodeInvalidWeight),
+			"codespace": DefaultCodespace,
+			"desc":      fmt.Sprintf("Invalid weight: weight cannot be %s than %d", data, weight),
+			"weight":    fmt.Sprintf("%d", weight),
+			"data":      data,
+		},
+	)
 	return sdkerrors.New(
 		DefaultCodespace,
 		CodeInvalidWeight,
-		fmt.Sprintf("Invalid weight: weight cannot be %s than %d", data, weight),
+		string(jsonData),
 	)
 }
 func ErrInvalidCoinToSend(denom string) *sdkerrors.Error {
+	jsonData, _ := json.Marshal(
+		map[string]string{
+			"code":      getCodeString(CodeInvalidCoinToSend),
+			"codespace": DefaultCodespace,
+			"desc":      fmt.Sprintf("Coin to send with symbol %s does not exist", denom),
+			"denom":     denom,
+		},
+	)
 	return sdkerrors.New(
 		DefaultCodespace,
 		CodeInvalidCoinToSend,
-		fmt.Sprintf("Coin to send with symbol %s does not exist", denom),
+		string(jsonData),
 	)
 }
 
 func ErrWalletAccountNotFound() *sdkerrors.Error {
+	jsonData, _ := json.Marshal(
+		map[string]string{
+			"code":      getCodeString(CodeWalletAccountNotFound),
+			"codespace": DefaultCodespace,
+			"desc":      "wallet account not found",
+		},
+	)
 	return sdkerrors.New(
 		DefaultCodespace,
 		CodeWalletAccountNotFound,
-		"wallet account not found",
+		string(jsonData),
 	)
 }
 
 func ErrInsufficientFunds(funds string) *sdkerrors.Error {
+	jsonData, _ := json.Marshal(
+		map[string]string{
+			"code":      getCodeString(CodeInsufficientFunds),
+			"codespace": DefaultCodespace,
+			"desc":      fmt.Sprintf("Insufficient funds: wanted = %s", funds),
+			"funds":     funds,
+		},
+	)
 	return sdkerrors.New(
 		DefaultCodespace,
 		CodeInsufficientFunds,
-		fmt.Sprintf("Insufficient funds: wanted = %s", funds),
+		string(jsonData),
 	)
 }
 
 func ErrDuplicateOwner(address sdk.AccAddress) *sdkerrors.Error {
+	jsonData, _ := json.Marshal(
+		map[string]string{
+			"code":      getCodeString(CodeDuplicateOwner),
+			"codespace": DefaultCodespace,
+			"desc":      fmt.Sprintf("Invalid owners: owner with address %s is duplicated", address),
+			"address":   address.String(),
+		},
+	)
 	return sdkerrors.New(
 		DefaultCodespace,
 		CodeDuplicateOwner,
-		fmt.Sprintf("Invalid owners: owner with address %s is duplicated", address),
+		string(jsonData),
 	)
+}
+
+func getCodeString(code CodeType) string {
+	return strconv.FormatInt(int64(code), 10)
 }
