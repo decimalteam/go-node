c1201bb5b43e965b18db0d47d9c4821b23584470
Михаил В <mvvershinin@gmail.com>
2021-05-28 14:43:17 +0700

    swap types codec

--- x/swap/types/codec.go
+++ x/swap/types/codec.go
@@ -1,6 +1,9 @@
 package types
 
-import "github.com/cosmos/cosmos-sdk/codec"
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
+)
 
 // RegisterCodec registers concrete types on codec
 func RegisterCodec(cdc *codec.LegacyAmino) {
@@ -15,6 +18,6 @@ var ModuleCdc *codec.LegacyAmino
 func init() {
 	ModuleCdc = codec.NewLegacyAmino()
 	RegisterCodec(ModuleCdc)
-	codec.RegisterCrypto(ModuleCdc)
+	cryptocodec.RegisterCrypto(ModuleCdc)
 	ModuleCdc.Seal()
 }
