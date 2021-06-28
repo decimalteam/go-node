d578ec0326f651245dfac54b107a21d9958eb2c3
Kirill Zharkov <sir.k.zharkov@gmail.com>
2021-06-28 17:11:38 +0700

    [update] Remove tokenName

--- config/config.go
+++ config/config.go
@@ -9,7 +9,7 @@ import (
 const (
 
 	// DecimalVersion is integer version of the Decimal app.
-	DecimalVersion = "1.3.2"
+	DecimalVersion = "1.3.3"
 
 	// DecimalMainPrefix is the main prefix for all keys and addresses.
 	DecimalMainPrefix = "dx"
