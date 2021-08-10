38c07aa9f468b072f2a72e467856b501b1dc3d2c
Kirill Zharkov <sir.k.zharkov@gmail.com>
2021-08-10 08:39:46 +0300

    Fix TotalStake

--- x/validator/internal/keeper/validator.go
+++ x/validator/internal/keeper/validator.go
@@ -144,13 +144,48 @@ func (k Keeper) TotalStake(ctx sdk.Context, validator types.Validator) sdk.Int {
 	wg := sync.WaitGroup{}
 	wg.Add(len(delegations))
 	for _, del := range delegations {
-		go func(del exported.DelegationI) {
-			defer wg.Done()
-			defer func() {
-				if r := recover(); r != nil {
-					fmt.Printf("stacktrace from panic TotalStake: %s \n%s\n", r, string(debug.Stack()))
+		if ctx.BlockHeight() >= updates.Update3Block {
+			go func(del exported.DelegationI) {
+				defer wg.Done()
+				defer func() {
+					if r := recover(); r != nil {
+						fmt.Printf("stacktrace from panic TotalStake: %s \n%s\n", r, string(debug.Stack()))
+					}
+				}()
+				if del.GetCoin().Denom != k.BondDenom(ctx) {
+					coin, err := k.GetCoin(ctx, del.GetCoin().Denom)
+					if err != nil {
+						panic(err)
+					}
+					if ctx.BlockHeight() >= updates.Update2Block {
+						delegatedCoin := k.GetDelegatedCoin(ctx, del.GetCoin().Denom)
+						totalAmountCoin := formulas.CalculateSaleReturn(coin.Volume, coin.Reserve, coin.CRR, delegatedCoin)
+						del = del.SetTokensBase(totalAmountCoin.Mul(del.GetCoin().Amount.ToDec().Quo(delegatedCoin.ToDec()).TruncateInt()))
+					} else {
+						del = del.SetTokensBase(formulas.CalculateSaleReturn(coin.Volume, coin.Reserve, coin.CRR, del.GetCoin().Amount))
+					}
+					tokenBase := formulas.CalculateSaleReturn(coin.Volume, coin.Reserve, coin.CRR, del.GetCoin().Amount)
+					eventMutex.Lock()
+					ctx.EventManager().EmitEvent(sdk.NewEvent(
+						types.EventTypeCalcStake,
+						sdk.NewAttribute(types.AttributeKeyValidator, validator.ValAddress.String()),
+						sdk.NewAttribute(types.AttributeKeyDelegator, del.GetDelegatorAddr().String()),
+						sdk.NewAttribute(types.AttributeKeyCoin, del.GetCoin().String()),
+						sdk.NewAttribute(types.AttributeKeyStake, tokenBase.String()),
+					))
+					eventMutex.Unlock()
+					switch del := del.(type) {
+					case types.Delegation:
+						k.SetDelegation(ctx, del)
+					case types.DelegationNFT:
+						k.SetDelegationNFT(ctx, del)
+					}
 				}
-			}()
+				mutex.Lock()
+				total = total.Add(del.GetTokensBase())
+				mutex.Unlock()
+			}(del)
+		} else {
 			if del.GetCoin().Denom != k.BondDenom(ctx) {
 				coin, err := k.GetCoin(ctx, del.GetCoin().Denom)
 				if err != nil {
@@ -183,8 +218,13 @@ func (k Keeper) TotalStake(ctx sdk.Context, validator types.Validator) sdk.Int {
 			mutex.Lock()
 			total = total.Add(del.GetTokensBase())
 			mutex.Unlock()
-		}(del)
+		}
 	}
+
+	if ctx.BlockHeight() < updates.Update3Block {
+		wg.Wait()
+	}
+
 	return total
 }
 
