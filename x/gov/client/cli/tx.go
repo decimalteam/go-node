8a6f2d41bfbf8041f81d42cb2936cf39f77d1606
Kirill Zharkov <sir.k.zharkov@gmail.com>
2021-09-01 11:37:24 +0300

    [update] Add cli

--- x/gov/client/cli/tx.go
+++ x/gov/client/cli/tx.go
@@ -3,8 +3,10 @@ package cli
 import (
 	"bufio"
 	"fmt"
+	"github.com/cosmos/cosmos-sdk/x/gov/client/cli"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/context"
@@ -65,6 +67,7 @@ func GetTxCmd(storeKey string, cdc *codec.Codec) *cobra.Command {
 	govTxCmd.AddCommand(flags.PostCommands(
 		GetCmdSubmitProposal(cdc),
 		GetCmdVote(cdc),
+		GetCmdSubmitUpgradeProposal(cdc),
 	)...)
 
 	return govTxCmd
@@ -176,6 +179,99 @@ $ %s tx gov vote 1 yes --from mykey
 			return utils.GenerateOrBroadcastMsgs(cliCtx, txBldr, []sdk.Msg{msg})
 		},
 	}
+}
+
+const (
+	// TimeFormat specifies ISO UTC format for submitting the time for a new upgrade proposal
+	TimeFormat = "2006-01-02T15:04:05Z"
+
+	FlagUpgradeHeight = "upgrade-height"
+	FlagUpgradeTime   = "time"
+	FlagUpgradeInfo   = "info"
+)
+
+const (
+	flagProposalType = "type"
+)
+
+func parseArgsToContent(cmd *cobra.Command, name string, proposer sdk.AccAddress) (types.MsgSoftwareUpgradeProposal, error) {
+	title, err := cmd.Flags().GetString(FlagTitle)
+	if err != nil {
+		return types.MsgSoftwareUpgradeProposal{}, err
+	}
+
+	description, err := cmd.Flags().GetString(FlagDescription)
+	if err != nil {
+		return types.MsgSoftwareUpgradeProposal{}, err
+	}
+
+	height, err := cmd.Flags().GetInt64(FlagUpgradeHeight)
+	if err != nil {
+		return types.MsgSoftwareUpgradeProposal{}, err
+	}
+
+	timeStr, err := cmd.Flags().GetString(FlagUpgradeTime)
+	if err != nil {
+		return types.MsgSoftwareUpgradeProposal{}, err
+	}
+
+	if height != 0 && len(timeStr) != 0 {
+		return types.MsgSoftwareUpgradeProposal{}, fmt.Errorf("only one of --upgrade-time or --upgrade-height should be specified")
+	}
+
+	var upgradeTime time.Time
+	if len(timeStr) != 0 {
+		upgradeTime, err = time.Parse(TimeFormat, timeStr)
+		if err != nil {
+			return types.MsgSoftwareUpgradeProposal{}, err
+		}
+	}
+
+	info, err := cmd.Flags().GetString(FlagUpgradeInfo)
+	if err != nil {
+		return types.MsgSoftwareUpgradeProposal{}, err
+	}
+
+	plan := types.Plan{Name: name, Time: upgradeTime, Height: height, Info: info}
+	msg := types.NewSoftwareUpgradeProposal(title, description, plan, proposer)
+	return msg, nil
+}
+
+// GetCmdSubmitUpgradeProposal implements a command handler for submitting a software upgrade proposal transaction.
+func GetCmdSubmitUpgradeProposal(cdc *codec.Codec) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "software-upgrade [name] (--upgrade-height [height] | --upgrade-time [time]) (--upgrade-info [info]) [flags]",
+		Args:  cobra.ExactArgs(1),
+		Short: "Submit a software upgrade proposal",
+		Long: "Submit a software upgrade along with an initial deposit.\n" +
+			"Please specify a unique name and height OR time for the upgrade to take effect.\n" +
+			"You may include info to reference a binary download link, in a format compatible with: https://github.com/regen-network/cosmosd",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			name := args[0]
+
+			inBuf := bufio.NewReader(cmd.InOrStdin())
+			txBldr := auth.NewTxBuilderFromCLI(inBuf).WithTxEncoder(utils.GetTxEncoder(cdc))
+			cliCtx := context.NewCLIContextWithInput(inBuf).WithCodec(cdc)
+			from := cliCtx.GetFromAddress()
+
+			msg, err := parseArgsToContent(cmd, name, from)
+			if err != nil {
+				return err
+			}
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+			return utils.GenerateOrBroadcastMsgs(cliCtx, txBldr, []sdk.Msg{msg})
+		},
+	}
+
+	cmd.Flags().String(cli.FlagTitle, "", "title of proposal")
+	cmd.Flags().String(cli.FlagDescription, "", "description of proposal")
+	cmd.Flags().Int64(FlagUpgradeHeight, 0, "The height at which the upgrade must happen (not to be used together with --upgrade-time)")
+	cmd.Flags().String(FlagUpgradeTime, "", fmt.Sprintf("The time at which the upgrade must happen (ex. %s) (not to be used together with --upgrade-height)", TimeFormat))
+	cmd.Flags().String(FlagUpgradeInfo, "", "Optional info for the planned upgrade such as commit hash, etc.")
+
+	return cmd
 }
 
 // DONTCOVER
