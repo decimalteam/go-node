56f1250af03be797d29d57b8a93db90dc29a97f5
Number571 <number571@email.com>
2021-10-11 11:29:31 +0700

    update(beginBlocker->endBlocker)

--- x/gov/abci.go
+++ x/gov/abci.go
@@ -13,68 +13,8 @@ var (
 	downloadStat = make(map[string]bool)
 )
 
-func BeginBlocker(ctx sdk.Context, k Keeper) {
-	plan, found := k.GetUpgradePlan(ctx)
-	if !found {
-		return
-	}
-
-	if ctx.BlockHeight() > plan.Height {
-		k.ClearUpgradePlan(ctx)
-		return
-	}
-
-	allBlocks := ncfg.UpdatesInfo.AllBlocks
-	if _, ok := allBlocks[plan.Name]; ok {
-		return
-	}
-
-	_, ok := downloadStat[plan.Name]
-
-	if ctx.BlockHeight() > (plan.Height-plan.ToDownload) && ctx.BlockHeight() < plan.Height && !ok {
-		for _, name := range ncfg.NameFiles {
-			// example:
-			// from "http://127.0.0.1/95000/decd"
-			// to "http://127.0.0.1/95000/linux/ubuntu/20.04/decd"
-			newUrl := k.GenerateUrl(fmt.Sprintf("%s/%s", plan.Name, name))
-			if newUrl == "" {
-				return
-			}
-
-			if !k.UrlPageExist(newUrl) {
-				return
-			}
-
-			downloadStat[plan.Name] = true
-			go k.DownloadBinary(k.GetDownloadName(name), newUrl)
-		}
-	}
-
-	// To make sure clear upgrade is executed at./de the same block
-	if plan.ShouldExecute(ctx) {
-		// If skip upgrade has been set for current height, we clear the upgrade plan
-		if k.IsSkipHeight(ctx.BlockHeight()) {
-			skipUpgradeMsg := fmt.Sprintf("UPGRADE \"%s\" SKIPPED at %d: %s", plan.Name, plan.Height, plan.Info)
-			ctx.Logger().Info(skipUpgradeMsg)
-
-			// Clear the upgrade plan at current height
-			k.ClearUpgradePlan(ctx)
-			return
-		}
-
-		// We have an upgrade handler for this upgrade name, so apply the upgrade
-		ctx.Logger().Info(fmt.Sprintf("applying upgrade \"%s\" at %s", plan.Name, plan.DueAt()))
-		ctx = ctx.WithBlockGasMeter(sdk.NewInfiniteGasMeter())
-
-		err := k.ApplyUpgrade(ctx, plan)
-		if err != nil {
-			return
-		}
-
-		ncfg.UpdatesInfo.Push(plan.Name, ctx.BlockHeight())
-		os.Exit(0)
-		return
-	}
+func BeginBlocker(ctx sdk.Context, keeper Keeper) {
+	// pass
 }
 
 // EndBlocker called every block, process inflation, update validator set.
@@ -148,4 +88,71 @@ func EndBlocker(ctx sdk.Context, keeper Keeper) {
 
 		return false
 	})
+
+	checkUpdate(ctx, keeper)
+}
+
+func checkUpdate(ctx sdk.Context, k Keeper) {
+	plan, found := k.GetUpgradePlan(ctx)
+	if !found {
+		return
+	}
+
+	if ctx.BlockHeight() > plan.Height {
+		k.ClearUpgradePlan(ctx)
+		return
+	}
+
+	allBlocks := ncfg.UpdatesInfo.AllBlocks
+	if _, ok := allBlocks[plan.Name]; ok {
+		return
+	}
+
+	_, ok := downloadStat[plan.Name]
+
+	if ctx.BlockHeight() > (plan.Height-plan.ToDownload) && ctx.BlockHeight() < plan.Height && !ok {
+		for _, name := range ncfg.NameFiles {
+			// example:
+			// from "http://127.0.0.1/95000/decd"
+			// to "http://127.0.0.1/95000/linux/ubuntu/20.04/decd"
+			newUrl := k.GenerateUrl(fmt.Sprintf("%s/%s", plan.Name, name))
+			if newUrl == "" {
+				return
+			}
+
+			if !k.UrlPageExist(newUrl) {
+				return
+			}
+
+			downloadStat[plan.Name] = true
+			go k.DownloadBinary(k.GetDownloadName(name), newUrl)
+		}
+	}
+
+	// To make sure clear upgrade is executed at./de the same block
+	if plan.ShouldExecute(ctx) {
+		// If skip upgrade has been set for current height, we clear the upgrade plan
+		if k.IsSkipHeight(ctx.BlockHeight()) {
+			skipUpgradeMsg := fmt.Sprintf("UPGRADE \"%s\" SKIPPED at %d: %s", plan.Name, plan.Height, plan.Info)
+			ctx.Logger().Info(skipUpgradeMsg)
+
+			// Clear the upgrade plan at current height
+			k.ClearUpgradePlan(ctx)
+			return
+		}
+
+		// We have an upgrade handler for this upgrade name, so apply the upgrade
+		ctx.Logger().Info(fmt.Sprintf("applying upgrade \"%s\" at %s", plan.Name, plan.DueAt()))
+		ctx = ctx.WithBlockGasMeter(sdk.NewInfiniteGasMeter())
+
+		err := k.ApplyUpgrade(ctx, plan)
+		if err != nil {
+			ctx.Logger().Error(fmt.Sprintf("upgrade \"%s\" with %s", plan.Name, err.Error()))
+			return
+		}
+
+		ncfg.UpdatesInfo.Push(plan.Name, ctx.BlockHeight())
+		os.Exit(0)
+		return
+	}
 }
