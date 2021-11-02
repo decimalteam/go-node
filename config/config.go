0be3d4d27245047fa613df636c94fd8cabe7ed5a
Number571 <number571@email.com>
2021-11-02 18:30:03 +0700

    [update] delete event for tests

--- config/config.go
+++ config/config.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	// DecimalVersion is integer version of the Decimal app.
-	DecimalVersion = "0.9.35"
+	DecimalVersion = "0.9.36"
 
 	// DecimalMainPrefix is the main prefix for all keys and addresses.
 	DecimalMainPrefix = "dx"
