9132957165a8c223ec2541cb5cd45ed3b5bc73e7
NOPandJMP <kirill_zaycev_0004@mail.ru>
2021-11-08 14:34:17 +0700

    multisend work

--- x/coin/client/cli/tx_multisend.go
+++ x/coin/client/cli/tx_multisend.go
@@ -1,7 +1,7 @@
 package cli
 
 import (
-	cliUtils "bitbucket.org/decimalteam/go-node/x/coin/client/utils"
+	//cliUtils "bitbucket.org/decimalteam/go-node/x/coin/client/utils"
 	types2 "bitbucket.org/decimalteam/go-node/x/coin/types"
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
@@ -9,52 +9,6 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/spf13/cobra"
 )
-
-/*func GetCmdMultiSendCoin(cdc *codec.LegacyAmino) *cobra.Command {
-	return &cobra.Command{
-		Use:   "multisend [coin receiver] [coin receiver] ...",
-		Short: "Multisend coin",
-		Args:  cobra.MinimumNArgs(2),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			clientCtx := client.GetClientContextFromCmd(cmd).WithLegacyAmino(cdc)
-
-			sends := make([]types2.Send, len(args)/2)
-			coins := make([]sdk.Coin, len(args)/2)
-
-			for i, value := range args {
-				if i%2 == 0 {
-					coin, err := sdk.ParseCoinNormalized(value)
-					if err != nil {
-						return err
-					}
-					sends[i/2].Coin = coin
-					coins[i/2] = coin
-				} else {
-					sends[i/2].Receiver = value
-				}
-			}
-
-			msg := types2.NewMsgMultiSendCoin(clientCtx.GetFromAddress(), sends)
-
-			// Check if enough balance
-			balance, err := cliUtils.GetAccountCoins(clientCtx, clientCtx.GetFromAddress())
-			if err != nil {
-				return err
-			}
-
-			if !balance.IsAllGTE(coins) {
-				var wantFunds string
-				for _, send := range sends {
-					wantFunds += send.Coin.String() + ", "
-				}
-				wantFunds = wantFunds[:len(wantFunds)-2]
-				return types2.ErrInsufficientFunds(wantFunds, balance.String())
-			}
-
-			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
-		},
-	}
-}*/
 
 
 func GetCmdMultiSendCoin() *cobra.Command {
@@ -66,6 +20,9 @@ func GetCmdMultiSendCoin() *cobra.Command {
 			cmd.Flags().Set(flags.FlagFrom, args[0])
 
 			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
 			//We need this in order not to take the first element - [from]
 			args2 := args[1:]
 
@@ -88,7 +45,7 @@ func GetCmdMultiSendCoin() *cobra.Command {
 			msg := types2.NewMsgMultiSendCoin(clientCtx.GetFromAddress(), sends)
 
 			// Check if enough balance
-			balance, err := cliUtils.GetAccountCoins(clientCtx, clientCtx.GetFromAddress())
+			/*balance, err := cliUtils.GetAccountCoins(clientCtx, clientCtx.GetFromAddress())
 			if err != nil {
 				return err
 			}
@@ -100,7 +57,7 @@ func GetCmdMultiSendCoin() *cobra.Command {
 				}
 				wantFunds = wantFunds[:len(wantFunds)-2]
 				return types2.ErrInsufficientFunds(wantFunds, balance.String())
-			}
+			}*/
 			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
 		},
 	}
