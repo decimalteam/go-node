68525e993e301f72fea0af50c3be2bb93737a1f6
NOPandJMP <kirill_zaycev_04@mail.ru>
2021-11-10 02:49:23 -0800

    tx fix and test

--- x/gov/client/cli/tx.go
+++ x/gov/client/cli/tx.go
@@ -2,9 +2,9 @@ package cli
 
 import (
 	types2 "bitbucket.org/decimalteam/go-node/x/gov/types"
-	"bufio"
+	//"bufio"
 	"fmt"
-
+	"github.com/cosmos/cosmos-sdk/client/flags"
 
 	"github.com/cosmos/cosmos-sdk/client/tx"
 	"github.com/cosmos/cosmos-sdk/x/gov/client/cli"
@@ -65,16 +65,75 @@ func GetTxCmd(storeKey string, cdc *codec.LegacyAmino) *cobra.Command {
 	}
 
 	govTxCmd.AddCommand(
-		GetCmdSubmitProposal(cdc),
-		GetCmdVote(cdc),
-		GetCmdSubmitUpgradeProposal(cdc),
+		GetCmdSubmitProposal(),
+		GetCmdVote(),
+		GetCmdSubmitUpgradeProposal(),
 	)
 
 	return govTxCmd
 }
 
 // GetCmdSubmitProposal implements submitting a proposal transaction command.
-func GetCmdSubmitProposal(cdc *codec.LegacyAmino) *cobra.Command {
+func GetCmdSubmitProposal() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "submit-proposal [from]",
+		Short: "Submit a proposal along with an initial deposit",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Submit a proposal along with an initial deposit.
+Proposal title, description, type and deposit can be given directly or through a proposal JSON file.
+
+Example:
+$ %s tx gov submit-proposal --proposal="path/to/proposal.json" --from mykey
+
+Where proposal.json contains:
+
+{
+  "title": "Test Proposal",
+  "description": "My awesome proposal",
+  "voting_start_block": 10000,
+  "voting_end_block": 20000,
+}
+
+Which is equivalent to:
+
+$ %s tx gov submit-proposal --title="Test Proposal" --description="My awesome proposal" --voting_start_block 10000 --voting_end_block 20000 --from mykey
+`,
+				version.AppName, version.AppName,
+			),
+		),
+		Args: cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cmd.Flags().Set(flags.FlagFrom, args[0])
+
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			proposal, err := parseSubmitProposalFlags(cmd.Flags())
+			if err != nil {
+				return err
+			}
+
+			msg := types2.NewMsgSubmitProposal(types2.Content{
+				Title:       proposal.Title,
+				Description: proposal.Description,
+			}, clientCtx.GetFromAddress(), proposal.VotingStartBlock, proposal.VotingEndBlock)
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
+
+// GetCmdSubmitProposal implements submitting a proposal transaction command.
+/*func GetCmdSubmitProposal(cdc *codec.LegacyAmino) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "submit-proposal",
 		Short: "Submit a proposal along with an initial deposit",
@@ -129,9 +188,64 @@ $ %s tx gov submit-proposal --title="Test Proposal" --description="My awesome pr
 	cmd.Flags().String(FlagProposal, "", "proposal file path (if this path is given, other proposal flags are ignored)")
 
 	return cmd
-}
+}*/
 
 // GetCmdVote implements creating a new vote command.
+func GetCmdVote() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "vote [proposal-id] [option] [from]",
+		Args:  cobra.ExactArgs(3),
+		Short: "Vote for an active proposal, options: yes/no/abstain",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Submit a vote for an active proposal. You can
+find the proposal-id by running "%s query gov proposals".
+
+
+Example:
+$ %s tx gov vote 1 yes --from mykey
+`,
+				version.AppName, version.AppName,
+			),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cmd.Flags().Set(flags.FlagFrom, args[2])
+
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			from := clientCtx.GetFromAddress()
+
+			// validate that the proposal id is a uint
+			proposalID, err := strconv.ParseUint(args[0], 10, 64)
+			if err != nil {
+				return fmt.Errorf("proposal-id %s not a valid int, please input a valid proposal-id", args[0])
+			}
+
+			// Find out which vote option user chose
+			byteVoteOption, err := types2.VoteOptionFromString(govutils.NormalizeVoteOption(args[1]))
+			if err != nil {
+				return err
+			}
+
+			// Build vote message and run basic validation
+			msg := types2.NewMsgVote(sdk.ValAddress(from), proposalID, byteVoteOption)
+			err = msg.ValidateBasic()
+			if err != nil {
+				return err
+			}
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
+
+/*
 func GetCmdVote(cdc *codec.LegacyAmino) *cobra.Command {
 	return &cobra.Command{
 		Use:   "vote [proposal-id] [option]",
@@ -177,7 +291,7 @@ $ %s tx gov vote 1 yes --from mykey
 			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
 		},
 	}
-}
+}*/
 
 const (
 	// TimeFormat specifies ISO UTC format for submitting the time for a new upgrade proposal
@@ -242,7 +356,49 @@ func parseArgsToContent(cmd *cobra.Command, name string, proposer sdk.AccAddress
 }
 
 // GetCmdSubmitUpgradeProposal implements a command handler for submitting a software upgrade proposal transaction.
-func GetCmdSubmitUpgradeProposal(cdc *codec.LegacyAmino) *cobra.Command  {
+func GetCmdSubmitUpgradeProposal() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "software-upgrade [name] [from]  (--upgrade-height [height] | --upgrade-time [time]) (--upgrade-info [info]) [flags]",
+		Args:  cobra.ExactArgs(2),
+		Short: "Submit a software upgrade proposal",
+		Long: "Submit a software upgrade along with an initial deposit.\n" +
+			"Please specify a unique name and height OR time for the upgrade to take effect.\n" +
+			"You may include info to reference a binary download link, in a format compatible with: https://github.com/regen-network/cosmosd",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cmd.Flags().Set(flags.FlagFrom, args[1])
+			name := args[0]
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			from := clientCtx.GetFromAddress()
+
+			msg, err := parseArgsToContent(cmd, name, from)
+			if err != nil {
+				return err
+			}
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
+		},
+	}
+
+	cmd.Flags().String(cli.FlagTitle, "", "title of proposal")
+	cmd.Flags().String(cli.FlagDescription, "", "description of proposal")
+	cmd.Flags().Int64(FlagUpgradeHeight, 0, "The height at which the upgrade must happen (not to be used together with --upgrade-time)")
+	cmd.Flags().String(FlagUpgradeTime, "", fmt.Sprintf("The time at which the upgrade must happen (ex. %s) (not to be used together with --upgrade-height)", TimeFormat))
+	cmd.Flags().String(FlagUpgradeInfo, "", "Optional info for the planned upgrade such as commit hash, etc.")
+	cmd.Flags().Int64(FlagToDownload, 0, "How many blocks before the update you need to start downloading the new version")
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
+
+/*func GetCmdSubmitUpgradeProposal(cdc *codec.LegacyAmino) *cobra.Command  {
 	cmd := &cobra.Command{
 		Use:   "software-upgrade [name] (--upgrade-height [height] | --upgrade-time [time]) (--upgrade-info [info]) [flags]",
 		Args:  cobra.ExactArgs(1),
@@ -278,6 +434,6 @@ func GetCmdSubmitUpgradeProposal(cdc *codec.LegacyAmino) *cobra.Command  {
 	cmd.Flags().Int64(FlagToDownload, 0, "How many blocks before the update you need to start downloading the new version")
 
 	return cmd
-}
+}*/
 
 // DONTCOVER
