5674ca1f3baeb50976ce8e4c6d744e5efa4c5764
Number571 <number571@email.com>
2021-11-10 17:54:41 +0700

    backup

--- app/app.go
+++ app/app.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"os"
 
@@ -108,12 +109,10 @@ func NewInitApp(logger log.Logger, db dbm.DB, traceStore io.Writer, loadLatest b
 	bApp := bam.NewBaseApp(appName, logger, db, auth.DefaultTxDecoder(cdc), baseAppOptions...)
 
 	// Load file with updates info: last_block and all_blocks
-	config.UpdatesInfo.Load()
-
-	// err := config.UpdatesInfo.Load()
-	// if err != nil {
-	// 	panic(fmt.Sprintf("error: load file with updates '%s'", err.Error()))
-	// }
+	err := config.UpdatesInfo.Load()
+	if err != nil {
+		panic(fmt.Sprintf("error: load file with updates '%s'", err.Error()))
+	}
 
 	bApp.SetAppVersion(config.DecimalVersion)
 
@@ -284,7 +283,7 @@ func NewInitApp(logger log.Logger, db dbm.DB, traceStore io.Writer, loadLatest b
 	app.MountKVStores(keys)
 	app.MountTransientStores(tkeys)
 
-	err := app.LoadLatestVersion(app.keys[bam.MainStoreKey])
+	err = app.LoadLatestVersion(app.keys[bam.MainStoreKey])
 	if err != nil {
 		tos.Exit(err.Error())
 	}
