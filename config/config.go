0c12f511a52753c2ba2bfc066e43897df91d86b6
Number571 <number571@email.com>
2021-11-11 17:51:48 -0500

    fix(create_coin)

--- config/config.go
+++ config/config.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	// DecimalVersion is integer version of the Decimal app.
-	DecimalVersion = "1.3.38"
+	DecimalVersion = "1.3.39"
 
 	// DecimalMainPrefix is the main prefix for all keys and addresses.
 	DecimalMainPrefix = "dx"
