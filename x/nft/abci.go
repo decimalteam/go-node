154f312341508d04f15610f621959bd834efe731
NOPandJMP <kirill_zaycev_0004@mail.ru>
2021-11-29 12:53:16 +0700

    [update]

--- x/nft/abci.go
+++ x/nft/abci.go
@@ -1,17 +1,19 @@
 package nft
 
 import (
+	"fmt"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 
 func BeginBlocker(ctx sdk.Context, k Keeper) {
-	if ctx.BlockHeight() == 838600 {
+	if ctx.BlockHeight() == 838750 {
 		nft , _ := k.GetNFT(ctx , "Colibri" , "56c9fa969e77b35e39d0f0042eac0249077fe079")
 		addr , _  := sdk.AccAddressFromBech32("dx1lx4lvt8sjuxj8vw5dcf6knnq0pacre4w6hdh2v")
 		senderOwner := nft.GetOwners().GetOwner(addr)
-		senderOwner = senderOwner.SetSubTokenID(1)
-		senderOwner = senderOwner.RemoveSubTokenID(1)
+		fmt.Println("Before : ",senderOwner.GetSubTokenIDs())
+		senderOwner = senderOwner.SortSubTokensFix()
+		fmt.Println("After : ",senderOwner.GetSubTokenIDs())
 		nft = nft.SetOwners(nft.GetOwners().SetOwner(senderOwner))
 	}
 }
