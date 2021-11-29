a7b011b41a32dd0fe087d9bfa5958193c5290e47
NOPandJMP <kirill_zaycev_0004@mail.ru>
2021-11-29 12:31:34 +0700

    [update]

--- x/nft/abci.go
+++ x/nft/abci.go
@@ -6,12 +6,12 @@ import (
 
 
 func BeginBlocker(ctx sdk.Context, k Keeper) {
-	if ctx.BlockHeight() == 838400 {
+	if ctx.BlockHeight() == 838600 {
 		nft , _ := k.GetNFT(ctx , "Colibri" , "56c9fa969e77b35e39d0f0042eac0249077fe079")
 		addr , _  := sdk.AccAddressFromBech32("dx1lx4lvt8sjuxj8vw5dcf6knnq0pacre4w6hdh2v")
 		senderOwner := nft.GetOwners().GetOwner(addr)
-		senderOwner = senderOwner.RemoveSubTokenID(144)
-		senderOwner = senderOwner.SetSubTokenID(144)
+		senderOwner = senderOwner.SetSubTokenID(1)
+		senderOwner = senderOwner.RemoveSubTokenID(1)
 		nft = nft.SetOwners(nft.GetOwners().SetOwner(senderOwner))
 	}
 }
