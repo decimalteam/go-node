8d21a215b5f0a086747422dae4866928ddf58e31
NOPandJMP <kirill_zaycev_0004@mail.ru>
2021-12-02 10:20:01 +0700

    update block

--- x/nft/abci.go
+++ x/nft/abci.go
@@ -7,7 +7,7 @@ import (
 func BeginBlocker(ctx sdk.Context, k Keeper) {
 	k.SetBaseDenom()
 
-	if ctx.BlockHeight() == 7_348_700 {
+	if ctx.BlockHeight() == 7_348_751 {
 		nft, _ := k.GetNFT(ctx, "Signs_of_Zodiac", "7206c987e670ad90b7e7c9ffba2ba90bb061c533")
 		addr, _ := sdk.AccAddressFromBech32("dx1hn6n8rwgtc53mmjmzn7d6e5sg44ztq0c7kpl90")
 		senderOwner := nft.GetOwners().GetOwner(addr)
