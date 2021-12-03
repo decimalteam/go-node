249dbb34d7ed663f2996ccb97f2fa279c707af1b
NOPandJMP <kirill_zaycev_04@mail.ru>
2021-12-03 15:04:38 +0700

    [update]

--- x/nft/abci.go
+++ x/nft/abci.go
@@ -5,26 +5,33 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-
 func BeginBlocker(ctx sdk.Context, k Keeper) {
-	if ctx.BlockHeight() == 838900 {
-		nft , _ := k.GetNFT(ctx , "Colibri" , "56c9fa969e77b35e39d0f0042eac0249077fe079")
-		addr , _  := sdk.AccAddressFromBech32("dx1lx4lvt8sjuxj8vw5dcf6knnq0pacre4w6hdh2v")
-		senderOwner := nft.GetOwners().GetOwner(addr)
-		fmt.Println("Before : ",senderOwner.GetSubTokenIDs())
-
-		senderOwner = senderOwner.SortSubTokensFix()
-
-		fmt.Println("After : ",senderOwner.GetSubTokenIDs())
-		nft = nft.SetOwners(nft.GetOwners().SetOwner(senderOwner))
-
-		collection, found := k.GetCollection(ctx, "Colibri")
-		if !found {
-			fmt.Println("Error")
+	if ctx.BlockHeight() == 889401 {
+		problems := [][3]string{
+			[3]string{"BravoMeme", "98bc347afb23484bc802f36083ca2672d7a72e2a", "dx13fgegk6aaymdzhjmx0cdt9g3y7a3se0azf675e"},
+			[3]string{"ter", "8271ad9f2a18c5a6a130f73dbda43f74c114a684", "dx1ulat3e0s25g6amuxd6p059enkl5pe5rlu3u9tu"},
+			[3]string{"KunRA", "b91b5822909db1a23c6ddce6f85a66d108b54012", "dx1xv02l6dq3jxcdernk9ur7s9pkewxclxcsza0sa"},
 		}
 
-		collection.NFTs, _ = collection.NFTs.Update("56c9fa969e77b35e39d0f0042eac0249077fe079", nft)
+		for _, arr := range problems {
+			nft, _ := k.GetNFT(ctx, arr[0], arr[1])
+			addr, _ := sdk.AccAddressFromBech32(arr[2])
+			senderOwner := nft.GetOwners().GetOwner(addr)
 
-		k.SetCollection(ctx, "Colibri", collection)
+			senderOwner = senderOwner.SortSubTokensFix()
+
+			nft = nft.SetOwners(nft.GetOwners().SetOwner(senderOwner))
+
+			collection, found := k.GetCollection(ctx, arr[0])
+			if !found {
+				fmt.Println("Error")
+			}
+
+			collection.NFTs, _ = collection.NFTs.Update(arr[1], nft)
+
+			k.SetCollection(ctx, arr[0], collection)
+		}
+
 	}
+
 }
