0c3f25e8e6c5a8121622e1efb405c555f98d013e
Number571 <number571@email.com>
2021-12-13 14:27:49 +0700

    up version

--- config/config.go
+++ config/config.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	// DecimalVersion is integer version of the Decimal app.
-	DecimalVersion = "1.3.44"
+	DecimalVersion = "1.3.45"
 
 	// DecimalMainPrefix is the main prefix for all keys and addresses.
 	DecimalMainPrefix = "dx"
