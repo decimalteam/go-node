dc2af63378e59b2a74ca1c4cf2affb99f1ba6783
NOPandJMP <kirill_zaycev_04@mail.ru>
2021-12-14 12:15:37 +0700

    up version 1.2.5

--- config/config.go
+++ config/config.go
@@ -12,7 +12,7 @@ import (
 const (
 
 	// DecimalVersion is integer version of the Decimal app.
-	DecimalVersion = "1.2.4"
+	DecimalVersion = "1.2.5"
 
 	// DecimalMainPrefix is the main prefix for all keys and addresses.
 	DecimalMainPrefix = "dx"
