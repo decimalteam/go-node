ff375c8c1189b6d26d133de89cf59faed9e69b5c
Number571 <number571@email.com>
2021-12-29 17:05:16 +0700

    add new route

--- cmd/deccli/main.go
+++ cmd/deccli/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"path"
 
+	"github.com/gorilla/mux"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
@@ -12,14 +17,17 @@ import (
 	"github.com/tendermint/tendermint/libs/cli"
 
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/keys"
 	"github.com/cosmos/cosmos-sdk/client/lcd"
 	"github.com/cosmos/cosmos-sdk/client/rpc"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/rest"
 	"github.com/cosmos/cosmos-sdk/version"
 	authcmd "github.com/cosmos/cosmos-sdk/x/auth/client/cli"
 	authrest "github.com/cosmos/cosmos-sdk/x/auth/client/rest"
+	"github.com/cosmos/cosmos-sdk/x/auth/types"
 	bankcmd "github.com/cosmos/cosmos-sdk/x/bank/client/cli"
 
 	"bitbucket.org/decimalteam/go-node/app"
@@ -80,6 +88,110 @@ func registerRoutes(rs *lcd.RestServer) {
 	client.RegisterRoutes(rs.CliCtx, rs.Mux)
 	app.ModuleBasics.RegisterRESTRoutes(rs.CliCtx, rs.Mux)
 	authrest.RegisterTxRoutes(rs.CliCtx, rs.Mux)
+	registerCustomRoutes(rs.CliCtx, rs.Mux)
+}
+
+func registerCustomRoutes(cliCtx context.CLIContext, r *mux.Router) {
+	r.HandleFunc(
+		"/auth/accounts_with_unconfirmed_nonce/{address}", QueryAccountWithUnconfirmedNonceRequestHandlerFn(cliCtx),
+	).Methods("GET")
+}
+
+// Edited QueryAccountRequestHandlerFn
+func QueryAccountWithUnconfirmedNonceRequestHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		bech32addr := vars["address"]
+
+		addr, err := sdk.AccAddressFromBech32(bech32addr)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		cliCtx, ok := rest.ParseQueryHeightOrReturnBadRequest(w, cliCtx, r)
+		if !ok {
+			return
+		}
+
+		accGetter := types.NewAccountRetriever(cliCtx)
+
+		account, height, err := accGetter.GetAccountWithHeight(addr)
+		if err != nil {
+			if err := accGetter.EnsureExists(addr); err != nil {
+				cliCtx = cliCtx.WithHeight(height)
+				rest.PostProcessResponse(w, cliCtx, types.BaseAccount{})
+				return
+			}
+
+			rest.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		// get unconfirmed nonce
+		nonce := account.GetSequence()
+		countTXs, _ := parseCountTXs(account.GetAddress())
+
+		// unconfirmed nonce = confirmed nonce + count txs from account
+		err = account.SetSequence(nonce + countTXs)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		cliCtx = cliCtx.WithHeight(height)
+		rest.PostProcessResponse(w, cliCtx, account)
+	}
+}
+
+func parseCountTXs(address sdk.AccAddress) (uint64, error) {
+	type result struct {
+		Result struct {
+			Txs [][]byte `json:"txs"`
+		} `json:"result"`
+	}
+
+	const (
+		hostURL = "http://localhost"
+		portRPC = 26657
+		limit   = 1000
+	)
+
+	resp, err := http.Get(fmt.Sprintf("%s:%d/unconfirmed_txs?limit=%d", hostURL, portRPC, limit))
+	if err != nil {
+		return 0, err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return 0, err
+	}
+
+	res := result{}
+	err = json.Unmarshal(body, &res)
+	if err != nil {
+		return 0, err
+	}
+
+	cdc := app.MakeCodec()
+	cdc.Seal()
+
+	count := uint64(0)
+	for _, tx := range res.Result.Txs {
+		msg, err := types.DefaultTxDecoder(cdc)(tx)
+		if err != nil {
+			return 0, err
+		}
+		for _, gmsg := range msg.GetMsgs() {
+			for _, signer := range gmsg.GetSigners() {
+				if bytes.Equal(signer, address) {
+					count++
+				}
+			}
+		}
+	}
+
+	return count, nil
 }
 
 func queryCmd(cdc *amino.Codec) *cobra.Command {
