d623585291591e04a4576807f4d31a2149630a36
Isteb4k <rakitin.dmit@gmail.com>
2022-01-21 17:01:29 +0700

    update version

--- config/config.go
+++ config/config.go
@@ -12,7 +12,7 @@ import (
 const (
 
 	// DecimalVersion is integer version of the Decimal app.
-	DecimalVersion = "1.2.6"
+	DecimalVersion = "1.2.7"
 
 	// DecimalMainPrefix is the main prefix for all keys and addresses.
 	DecimalMainPrefix = "dx"
