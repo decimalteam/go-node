c068b26995bae5784411cd4e14120753781d9b1b
Vyacheslav Okulichev <khirn01@gmail.com>
2022-02-24 11:53:11 +0700

    feature/21666 fix: mixed caps variables' names

--- config/updates_test.go
+++ config/updates_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestLoadFromOldConfig(t *testing.T) {
-	oldconfig := []byte(`{"last_update":8123401,
+	oldConfig := []byte(`{"last_update":8123401,
 	"all_updates":{"https://repo.decimalchain.com/7010801":7010801,
 	"https://repo.decimalchain.com/7098701":7098701,
 	"https://repo.decimalchain.com/7348701":7348701,
@@ -17,24 +17,24 @@ func TestLoadFromOldConfig(t *testing.T) {
 	"https://repo.decimalchain.com/7980901":7980901,
 	"https://repo.decimalchain.com/8037701":8037701,
 	"https://repo.decimalchain.com/8123401":8123401}}`)
-	updinf := NewUpdatesInfo("")
-	r := bytes.NewReader(oldconfig)
-	updinf.load(r)
-	assert.Equal(t, 8, len(updinf.AllBlocks), "old AllBlock must be 8")
-	assert.Equal(t, int64(8123401), updinf.LastBlock, "LastBlock must be in safe")
+	updInf := NewUpdatesInfo("")
+	r := bytes.NewReader(oldConfig)
+	updInf.load(r)
+	assert.Equal(t, 8, len(updInf.AllBlocks), "old AllBlock must be 8")
+	assert.Equal(t, int64(8123401), updInf.LastBlock, "LastBlock must be in safe")
 }
 
 func TestSaveLoad(t *testing.T) {
-	updinf := NewUpdatesInfo("")
-	updinf.PushNewPlanHeight(1)
-	updinf.AddExecutedPlan("1", 1)
-	updinf.AddExecutedPlan("2", 1)
-	updinf.AddExecutedPlan("3", 1)
+	updInf := NewUpdatesInfo("")
+	updInf.PushNewPlanHeight(1)
+	updInf.AddExecutedPlan("1", 1)
+	updInf.AddExecutedPlan("2", 1)
+	updInf.AddExecutedPlan("3", 1)
 	tmp := make([]byte, 0)
 	buf := bytes.NewBuffer(tmp)
-	updinf.save(buf)
+	updInf.save(buf)
 	newinf := NewUpdatesInfo("")
 	newinf.load(buf)
-	assert.Equal(t, updinf.AllBlocks, newinf.AllBlocks, "AllBlocks must be same")
-	assert.Equal(t, updinf.LastBlock, newinf.LastBlock, "LastBlock must be same")
+	assert.Equal(t, updInf.AllBlocks, newinf.AllBlocks, "AllBlocks must be same")
+	assert.Equal(t, updInf.LastBlock, newinf.LastBlock, "LastBlock must be same")
 }
