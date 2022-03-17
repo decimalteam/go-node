01f4829efebd524482fb1661bfd3e004a9b61aad
Igor Sokolov <wardencliffe.sis@gmail.com>
2022-03-17 19:40:14 +0700

    hotfix/22943 fix: fix in gov tests.

--- utils/updates/updates.go
+++ utils/updates/updates.go
@@ -2,4 +2,4 @@ package updates
 
 // This update suddenly appeared since there was no way to move
 // gov and swap storage records to use correct useful prefixes in their keys
-const Update14Block = 9999999 // TODO: Choose the block before updating
+const Update14Block = 10000000000000 // TODO: Choose the block before updating
