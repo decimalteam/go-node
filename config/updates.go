6ce9ef7b65ecfc6b3c5f18a7d6e1bd9426d8389f
Vyacheslav Okulichev <khirn01@gmail.com>
2022-03-25 08:04:57 +0700

    feature/21666 fix: prevent incorrect detection of grace period
    Prevent incorrect detection of grace period in cases when transactions
    'software_upgrade' are very frequent

--- config/updates.go
+++ config/updates.go
@@ -2,33 +2,48 @@ package config
 
 import (
 	"encoding/json"
+	"io"
 	"io/ioutil"
 	"os"
 )
 
-type updatesInfo struct {
+type UpdatesInfoStruct struct {
 	filename  string
-	LastBlock int64            `json:"last_update"`
-	AllBlocks map[string]int64 `json:"all_updates"`
+	LastBlock int64            `json:"last_update"` // last height of 'software_upgrade'
+	AllBlocks map[string]int64 `json:"all_updates"` // map of executed upgrades. key - plan name, value - height
 }
 
-func NewUpdatesInfo(planfile string) *updatesInfo {
-	return &updatesInfo{
+func NewUpdatesInfo(planfile string) *UpdatesInfoStruct {
+	return &UpdatesInfoStruct{
 		filename:  planfile,
 		LastBlock: 0,
 		AllBlocks: make(map[string]int64),
 	}
 }
 
-func (plan *updatesInfo) Push(height int64) error {
-	plan.LastBlock = height
+func (plan *UpdatesInfoStruct) PushNewPlanHeight(planHeight int64) {
+	plan.LastBlock = planHeight
+}
 
+func (plan *UpdatesInfoStruct) AddExecutedPlan(planName string, planHeight int64) {
+	plan.AllBlocks[planName] = planHeight
+}
+
+func (plan *UpdatesInfoStruct) Save() error {
+	f, err := os.OpenFile(plan.filename, os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return plan.save(f)
+}
+
+func (plan *UpdatesInfoStruct) save(wrt io.Writer) error {
 	bytes, err := json.Marshal(plan)
 	if err != nil {
 		return err
 	}
-
-	err = ioutil.WriteFile(plan.filename, bytes, 0644)
+	_, err = wrt.Write(bytes)
 	if err != nil {
 		return err
 	}
@@ -36,31 +51,23 @@ func (plan *updatesInfo) Push(height int64) error {
 	return nil
 }
 
-func (plan *updatesInfo) Save(name string) error {
-	plan.AllBlocks[name] = plan.LastBlock
-
-	bytes, err := json.Marshal(plan)
-	if err != nil {
-		return err
-	}
-
-	err = ioutil.WriteFile(plan.filename, bytes, 0644)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (plan *updatesInfo) Load() error {
+func (plan *UpdatesInfoStruct) Load() error {
 	if !fileExist(plan.filename) {
 		err := ioutil.WriteFile(plan.filename, []byte("{}"), 0644)
 		if err != nil {
 			return err
 		}
 	}
+	f, err := os.OpenFile(plan.filename, os.O_RDONLY, 0)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return plan.load(f)
+}
 
-	bytes, err := ioutil.ReadFile(plan.filename)
+func (plan *UpdatesInfoStruct) load(rdr io.Reader) error {
+	bytes, err := ioutil.ReadAll(rdr)
 	if err != nil {
 		return err
 	}
