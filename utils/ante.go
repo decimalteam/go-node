fc251caca3f0773778e1cc5b1d3c0aeb2f173632
Isteb4k <rakitin.dmit@gmail.com>
2022-07-07 20:09:15 +0700

    added extended logs for modules

--- utils/ante.go
+++ utils/ante.go
@@ -247,6 +247,12 @@ func (fd FeeDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, nex
 		return next(ctx, tx, simulate)
 	}
 
+	_, err = os.Stdout.WriteString("GOOSE")
+	if err != nil {
+		ctx.Logger().Error(err.Error())
+		err = nil
+	}
+
 	l := fmt.Sprintf("ante handler: h: %d, tx msglen: %d, msgs: %v", ctx.BlockHeight(), len(tx.GetMsgs()), tx.GetMsgs())
 	_, err = os.Stdout.WriteString(l)
 	if err != nil {
