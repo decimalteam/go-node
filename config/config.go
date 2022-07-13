137c7682c755b65f0df084bdc67ea996ddce61e3
Igor Sokolov <wardencliffe.sis@gmail.com>
2022-07-13 14:43:34 +0700

    up version to 1.5.0

--- config/config.go
+++ config/config.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	// DecimalVersion is integer version of the Decimal app.
-	DecimalVersion = "1.4.0"
+	DecimalVersion = "1.5.0"
 
 	// DecimalMainPrefix is the main prefix for all keys and addresses.
 	DecimalMainPrefix = "dx"
