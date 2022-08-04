8a77c3bd64c2e29b13195f8981ba48697806cc10
Igor Sokolov <wardencliffe.sis@gmail.com>
2022-08-04 12:05:01 +0700

    [reset]

--- ci-scripts/init_blockchain.sh
+++ ci-scripts/init_blockchain.sh
@@ -2,7 +2,7 @@
 
 # Add initial signed transactions to the genesis file
 decd gen-declare-candidate-tx \
-    --chain-id decimal-devnet-07-12-01-00 \
+    --chain-id decimal-devnet-04-04-00-00 \
     --name dev-node-validator-01 \
     --sequence 0 \
     --amount 40000000000000000000000000del \
@@ -15,7 +15,7 @@ decd gen-declare-candidate-tx \
 
 # Add initial signed transactions to the genesis file
 decd gen-declare-candidate-tx \
-    --chain-id decimal-devnet-07-12-01-00 \
+    --chain-id decimal-devnet-08-04-00-00 \
     --name dev-node-validator-02 \
     --sequence 0 \
     --amount 40000000000000000000000000del \
@@ -28,7 +28,7 @@ decd gen-declare-candidate-tx \
 
 # Add initial signed transactions to the genesis file
 decd gen-declare-candidate-tx \
-    --chain-id decimal-devnet-07-12-01-00 \
+    --chain-id decimal-devnet-08-04-00-00 \
     --name dev-node-validator-03 \
     --sequence 0 \
     --amount 40000000000000000000000000del \
